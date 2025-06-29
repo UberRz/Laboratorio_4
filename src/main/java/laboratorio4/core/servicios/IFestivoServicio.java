0486ecaa6d1cb128f199ab4c24a24c26cdcfb9f8
uberrz <u3er07@gmail.com>
2025-06-29 13:44:03 -0500

    se crean las interfaces para los servicios festivo y tipo

---
+++ src/main/java/laboratorio4/core/servicios/IFestivoServicio.java
@@ -0,0 +1,24 @@
+package laboratorio4.core.servicios;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import laboratorio4.core.dominio.entidades.Festivo;
+
+@Service
+public interface IFestivoServicio {
+
+    List<Festivo> listar();
+
+    Festivo obtener(int id);
+
+    List<Festivo> buscar(String dato);
+
+    Festivo agregar(Festivo festivo);
+
+    Festivo modificar(Festivo festivo);
+
+    boolean eliminar(int id);
+
+}
---
+++ src/main/java/laboratorio4/core/servicios/ITipoServicio.java
@@ -0,0 +1,24 @@
+package laboratorio4.core.servicios;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import laboratorio4.core.dominio.entidades.Tipo;
+
+@Service
+public interface ITipoServicio {
+
+    List<Tipo> listar();
+
+    Tipo obtener(int id);
+
+    List<Tipo> buscar(String dato);
+
+    Tipo agregar(Tipo tipo);
+
+    Tipo modificar(Tipo tipo);
+
+    boolean eliminar(int id);
+
+}
